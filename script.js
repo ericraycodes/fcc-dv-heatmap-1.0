bd618f4b71998f1bd8ce4cc2994ec8dee3924e1d
lasernoy <cloudy.nny07@gmail.com>
2024-06-26 13:33:30 +0800

    ordinal scale

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 
 
-// HEAT MAP
-function mapData(data) {
+/** HEAT MAP VISUALIZATION
+    * @parameter Array: the fetched dataset to visualize; array of objects.
+    * @library D3: loaded and accessed through a CDN.
+    */
+function mapData(dataset) {
   // console
   console.log("creating heat map...");
 
   // map dimensions
-  const w = 1200;
+  const w = 1500;
   const h = 500;
   const padTop = 10;
   const padBot = 50;
@@ -18,12 +21,30 @@ function mapData(data) {
     .append("svg")
     .attr("width", w)
     .attr("height", h);
+
+  // scale Y axis
+  const yScale = d3.scaleOrdinal()
+    .domain([d3.min(dataset, d => d.month), d3.max(dataset, d => d.month)])
+    .range([h - padBot, padTop]);
+
+  // scale X axis
+  const xScale = d3.scaleOrdinal()
+    .domain([0, dataset.length])
+    .range([padLeft, w - padRight]);
 }
 
 
 
 
-// REQUEST DATA ON 'DOMCONTENTLOADED'
+/** EVENT LISTENER: REQUEST DATA ON 'DOMCONTENTLOADED'
+    * @event 'DOMContentLoaded'
+    *
+    * This script is deferred. The event will fire when
+    * the D3 library and other resources are loaded successfully.
+    *
+    * This event will run a call back to request external dataset.
+    * A function is run to visualize dataset when loaded.
+    */
 document.addEventListener("DOMContentLoaded", () => {
   // console DOM loaded
   console.log("DOM loaded!");
