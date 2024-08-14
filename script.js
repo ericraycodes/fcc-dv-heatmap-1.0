39574b98343b75fdf74ca24407ed906e35b4f8e9
lasernoy <cloudy.nny07@gmail.com>
2024-08-14 11:47:32 +0800

    rect graphed

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ function mapData(dataset) {
   // console.log("dataset.monthlyVariance:", dataset.monthlyVariance);
   // console.log(typeof dataset.monthlyVariance[0].year);
 
+
   // data info
   const months = [
     "January", "February", "March", "April", "May", "June", "July",
@@ -27,9 +28,11 @@ function mapData(dataset) {
   } while (year <= yearMax)
   console.log("yearRange:",yearRange);
 
+
   // heading
   const h1 = d3.select("#title").text("Monthly Global Land Surface Temperature");
-  const h2 = d3.select("#description").html(`${yearMin} - ${yearMax}: base temperature ${dataset.baseTemperature}&deg;C`)
+  const h2 = d3.select("#description").html(`${yearMin} - ${yearMax}: base temperature ${dataset.baseTemperature}&deg;C`);
+
 
   // map dimensions
   const w = 1500;
@@ -39,11 +42,13 @@ function mapData(dataset) {
   const padLeft = 100;
   const padRight = 10;
 
+
   // svg
   const svg = d3.select("#heatmap-container")
     .append("svg")
     .attr("width", w)
     .attr("height", h);
+
 
   // Y axis
   const yScale = d3.scaleBand()
@@ -54,16 +59,50 @@ function mapData(dataset) {
     .attr("transform", `translate(${padLeft}, 0)`)
     .call(yAxis)
     .attr("id", "y-axis");
+
   // X axis
   const xScale = d3.scaleBand()
     .domain(yearRange)
     .range([padLeft, w - padRight]);
   const xAxis = d3.axisBottom(xScale)
-    .tickValues(xScale.domain().filter(yr => yr%10===0))
+    .tickValues(xScale.domain().filter(yr => yr%10===0));
   svg.append("g")
     .attr("transform", `translate(0, ${h - padBot})`)
     .call(xAxis)
     .attr("id", "x-axis");
+
+
+  // heat colors: cold - hot : #3A1078, #6482AD, #C63C51, #921A40
+  const tempColors = ['#3A1078', '#6482AD', '#C63C51', '#921A40'];
+
+  // temperature data
+  const tempMin = d3.min(dataset.monthlyVariance, d => dataset.baseTemperature + d.variance);
+  const tempMax = d3.max(dataset.monthlyVariance, d => dataset.baseTemperature + d.variance);
+  console.log("temperature range:", tempMin, '-', tempMax);
+
+  // temp - color scale
+  const tempColorScale = d3.scaleQuantize()
+    .domain([tempMin, tempMax])
+    .range(tempColors);
+  console.log("color scale:", tempColorScale(tempMax));
+
+  console.log("x band:", xScale.bandwidth());
+  console.log("y band:", yScale.bandwidth());
+
+  // heat cells
+  svg.selectAll("rect")
+    .data(dataset.monthlyVariance)
+    .enter()
+    .append("rect")
+    .attr("class", "cell")
+    .attr("data-year", d => d.year)
+    .attr("data-month", d => months[d.month - 1])    
+    .attr("data-temp", d => dataset.baseTemperature + d.variance)
+    .attr("width", xScale.bandwidth())
+    .attr("height", yScale.bandwidth())
+    .attr("x", d => xScale(d.year))
+    .attr("y", d => yScale(months[d.month - 1]))
+    .attr("fill", d => tempColorScale(dataset.baseTemperature + d.variance))
 }
 
 
